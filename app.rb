107a9952b2e28423c479a6732c0ea05593ef9547
Gaston Ramos <ramos.gaston@gmail.com>
2016-03-14 10:19:04 -0300

    First working example.

--- app.rb
+++ app.rb
@@ -1,8 +1,27 @@
 require 'cuba'
 require 'mote'
 require 'mote/render'
+require 'swift_client'
+require 'json'
 
 Cuba.plugin Mote::Render
+
+o_storage   = JSON.parse ENV['VCAP_SERVICES']
+credentials = o_storage['Object-Storage'].first['credentials']
+
+swift_client = SwiftClient.new(
+  :auth_url          => "#{ credentials['auth_url'] }/v3",
+  :username          => credentials['username'],
+  :password          => credentials['password'],
+  :domain_id         => credentials['domainId'],
+  :project_name      => credentials['project'],
+  :project_domain_id =>  credentials['domainId'],
+  :storage_url       => "https://dal.objectstorage.open.softlayer.com/v1/AUTH_#{ credentials['projectId'] }"
+)
+
+puts swift_client.auth_token.inspect
+puts swift_client.storage_url
+puts swift_client.get_containers.inspect
 
 Cuba.define do
 
@@ -11,8 +30,5 @@ Cuba.define do
       render 'upload'
     end
 
-    on post do
-
-    end
   end
 end
